7b782e85ab3832a9b80772890fc142207bd283f7
JayhawkZombie <kurt_slagle@yahoo.com>
2015-05-09 21:03:41 -0500

    More Updates to timetable and RTM.  Database changed in testing.

--- debug/moc_mainwindow.cpp
+++ debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[20];
-    char stringdata[310];
+    QByteArrayData data[21];
+    char stringdata[322];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,30 +33,31 @@ QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
 QT_MOC_LITERAL(1, 11, 23), // "on_logoutButton_clicked"
 QT_MOC_LITERAL(2, 35, 0), // ""
 QT_MOC_LITERAL(3, 36, 22), // "on_loginButton_clicked"
-QT_MOC_LITERAL(4, 59, 12), // "exitFunction"
-QT_MOC_LITERAL(5, 72, 15), // "registerAccount"
-QT_MOC_LITERAL(6, 88, 10), // "insertUser"
-QT_MOC_LITERAL(7, 99, 11), // "std::string"
-QT_MOC_LITERAL(8, 111, 13), // "invalidDialog"
-QT_MOC_LITERAL(9, 125, 12), // "authenticate"
-QT_MOC_LITERAL(10, 138, 12), // "enableStatus"
-QT_MOC_LITERAL(11, 151, 20), // "on_addButton_clicked"
-QT_MOC_LITERAL(12, 172, 17), // "updateAccountList"
-QT_MOC_LITERAL(13, 190, 10), // "addAccount"
-QT_MOC_LITERAL(14, 201, 13), // "deleteAccount"
-QT_MOC_LITERAL(15, 215, 23), // "on_deleteButton_clicked"
-QT_MOC_LITERAL(16, 239, 21), // "on_saveButton_clicked"
-QT_MOC_LITERAL(17, 261, 26), // "on_accountList_itemClicked"
-QT_MOC_LITERAL(18, 288, 16), // "QListWidgetItem*"
-QT_MOC_LITERAL(19, 305, 4) // "item"
+QT_MOC_LITERAL(4, 59, 11), // "loginDialog"
+QT_MOC_LITERAL(5, 71, 12), // "exitFunction"
+QT_MOC_LITERAL(6, 84, 15), // "registerAccount"
+QT_MOC_LITERAL(7, 100, 10), // "insertUser"
+QT_MOC_LITERAL(8, 111, 11), // "std::string"
+QT_MOC_LITERAL(9, 123, 13), // "invalidDialog"
+QT_MOC_LITERAL(10, 137, 12), // "authenticate"
+QT_MOC_LITERAL(11, 150, 12), // "enableStatus"
+QT_MOC_LITERAL(12, 163, 20), // "on_addButton_clicked"
+QT_MOC_LITERAL(13, 184, 17), // "updateAccountList"
+QT_MOC_LITERAL(14, 202, 10), // "addAccount"
+QT_MOC_LITERAL(15, 213, 13), // "deleteAccount"
+QT_MOC_LITERAL(16, 227, 23), // "on_deleteButton_clicked"
+QT_MOC_LITERAL(17, 251, 21), // "on_saveButton_clicked"
+QT_MOC_LITERAL(18, 273, 26), // "on_accountList_itemClicked"
+QT_MOC_LITERAL(19, 300, 16), // "QListWidgetItem*"
+QT_MOC_LITERAL(20, 317, 4) // "item"
 
     },
     "MainWindow\0on_logoutButton_clicked\0\0"
-    "on_loginButton_clicked\0exitFunction\0"
-    "registerAccount\0insertUser\0std::string\0"
-    "invalidDialog\0authenticate\0enableStatus\0"
-    "on_addButton_clicked\0updateAccountList\0"
-    "addAccount\0deleteAccount\0"
+    "on_loginButton_clicked\0loginDialog\0"
+    "exitFunction\0registerAccount\0insertUser\0"
+    "std::string\0invalidDialog\0authenticate\0"
+    "enableStatus\0on_addButton_clicked\0"
+    "updateAccountList\0addAccount\0deleteAccount\0"
     "on_deleteButton_clicked\0on_saveButton_clicked\0"
     "on_accountList_itemClicked\0QListWidgetItem*\0"
     "item"
@@ -69,7 +70,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      15,   14, // methods
+      16,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -77,30 +78,32 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   89,    2, 0x08 /* Private */,
-       3,    0,   90,    2, 0x08 /* Private */,
-       4,    0,   91,    2, 0x08 /* Private */,
-       5,    0,   92,    2, 0x08 /* Private */,
-       6,    2,   93,    2, 0x08 /* Private */,
-       8,    1,   98,    2, 0x08 /* Private */,
-       9,    2,  101,    2, 0x08 /* Private */,
-      10,    1,  106,    2, 0x08 /* Private */,
-      11,    0,  109,    2, 0x08 /* Private */,
-      12,    0,  110,    2, 0x08 /* Private */,
-      13,    0,  111,    2, 0x08 /* Private */,
-      14,    0,  112,    2, 0x08 /* Private */,
-      15,    0,  113,    2, 0x08 /* Private */,
-      16,    0,  114,    2, 0x08 /* Private */,
-      17,    1,  115,    2, 0x08 /* Private */,
+       1,    0,   94,    2, 0x08 /* Private */,
+       3,    0,   95,    2, 0x08 /* Private */,
+       4,    0,   96,    2, 0x08 /* Private */,
+       5,    0,   97,    2, 0x08 /* Private */,
+       6,    0,   98,    2, 0x08 /* Private */,
+       7,    2,   99,    2, 0x08 /* Private */,
+       9,    1,  104,    2, 0x08 /* Private */,
+      10,    2,  107,    2, 0x08 /* Private */,
+      11,    1,  112,    2, 0x08 /* Private */,
+      12,    0,  115,    2, 0x08 /* Private */,
+      13,    0,  116,    2, 0x08 /* Private */,
+      14,    0,  117,    2, 0x08 /* Private */,
+      15,    0,  118,    2, 0x08 /* Private */,
+      16,    0,  119,    2, 0x08 /* Private */,
+      17,    0,  120,    2, 0x08 /* Private */,
+      18,    1,  121,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 7, 0x80000000 | 7,    2,    2,
-    QMetaType::Void, 0x80000000 | 7,    2,
-    QMetaType::Void, 0x80000000 | 7, 0x80000000 | 7,    2,    2,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 8, 0x80000000 | 8,    2,    2,
+    QMetaType::Void, 0x80000000 | 8,    2,
+    QMetaType::Void, 0x80000000 | 8, 0x80000000 | 8,    2,    2,
     QMetaType::Void, QMetaType::Bool,    2,
     QMetaType::Void,
     QMetaType::Void,
@@ -108,7 +111,7 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 18,   19,
+    QMetaType::Void, 0x80000000 | 19,   20,
 
        0        // eod
 };
@@ -120,19 +123,20 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->on_logoutButton_clicked(); break;
         case 1: _t->on_loginButton_clicked(); break;
-        case 2: _t->exitFunction(); break;
-        case 3: _t->registerAccount(); break;
-        case 4: _t->insertUser((*reinterpret_cast< std::string(*)>(_a[1])),(*reinterpret_cast< std::string(*)>(_a[2]))); break;
-        case 5: _t->invalidDialog((*reinterpret_cast< std::string(*)>(_a[1]))); break;
-        case 6: _t->authenticate((*reinterpret_cast< std::string(*)>(_a[1])),(*reinterpret_cast< std::string(*)>(_a[2]))); break;
-        case 7: _t->enableStatus((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 8: _t->on_addButton_clicked(); break;
-        case 9: _t->updateAccountList(); break;
-        case 10: _t->addAccount(); break;
-        case 11: _t->deleteAccount(); break;
-        case 12: _t->on_deleteButton_clicked(); break;
-        case 13: _t->on_saveButton_clicked(); break;
-        case 14: _t->on_accountList_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 2: _t->loginDialog(); break;
+        case 3: _t->exitFunction(); break;
+        case 4: _t->registerAccount(); break;
+        case 5: _t->insertUser((*reinterpret_cast< std::string(*)>(_a[1])),(*reinterpret_cast< std::string(*)>(_a[2]))); break;
+        case 6: _t->invalidDialog((*reinterpret_cast< std::string(*)>(_a[1]))); break;
+        case 7: _t->authenticate((*reinterpret_cast< std::string(*)>(_a[1])),(*reinterpret_cast< std::string(*)>(_a[2]))); break;
+        case 8: _t->enableStatus((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 9: _t->on_addButton_clicked(); break;
+        case 10: _t->updateAccountList(); break;
+        case 11: _t->addAccount(); break;
+        case 12: _t->deleteAccount(); break;
+        case 13: _t->on_deleteButton_clicked(); break;
+        case 14: _t->on_saveButton_clicked(); break;
+        case 15: _t->on_accountList_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -163,13 +167,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 15)
+        if (_id < 16)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 15;
+        _id -= 16;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 15)
+        if (_id < 16)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 15;
+        _id -= 16;
     }
     return _id;
 }
